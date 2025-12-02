6cb340640a7d0f2764f73a6ccabf0cd725437565
Adittooo01 <aditto.rahman@northsouth.edu>
2025-12-03 03:53:43 +0600

    Initial commit

---
+++ Cart/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+
+app_name = 'cart'
+
+'''urlpatterns = [
+    path('', views.cart, name='cart'),
+    path('add-to-cart/', views.add_to_cart, name='add_to_cart'),
+    path('cart/', views.render_cart, name='cart'),
+    path('update-cart/', views.update_cart, name='update_cart'),
+]'''
+
+
+
+
+urlpatterns = [
+    path('', views.render_cart, name='render_cart'),  # ✅ now matches /cart/
+    path('add-to-cart/', views.add_to_cart, name='add_to_cart'),
+    path('update-cart/', views.update_cart, name='update_cart'),
+]
